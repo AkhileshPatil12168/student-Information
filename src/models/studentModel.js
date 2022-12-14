217536190cb1ee3b8facdf3447ea038c6eb4c393
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2022-12-15 02:49:05 +0530

    final

---
+++ src/models/studentModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const studentSchema = new mongoose.Schema(
+    {
+        fname: { type: String, require: true, trim: true },
+        lname: { type: String, require: true, trim: true },
+        subject: { type: Object },
+       
+        rollNo: { type: String, required: true },
+        teacherId: { type: ObjectId, ref: "Teacher", required: true },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Student", studentSchema);
+
+// subject :{"maths":50, "sci":60}
